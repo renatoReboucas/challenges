ad2f634c10959271be94a83b5872e76ee22fee09
Renato Rebouças <renato190698@gmail.com>
2019-05-26 21:28:16 -0300

    ndsnjsd

--- app/src/main/java/com/rr/challenges/ConversorActivity.kt
+++ app/src/main/java/com/rr/challenges/ConversorActivity.kt
@@ -2,7 +2,10 @@ package com.rr.challenges
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_conversor.*
 import utils.utilidade
@@ -13,26 +16,79 @@ class ConversorActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_conversor)
 
+        val temp = arrayListOf(
+            "Selecione uma temperatura:",
+            "Celsius",
+            "Kelvin",
+            "Fahrenheit"
+        )
+
+        val adpter = ArrayAdapter(this@ConversorActivity, android.R.layout.simple_spinner_item, temp)
+
+        spinner.adapter = adpter
+
+        spinner.setOnItemSelectedListener(object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+//                txt_result.text = "Você escolheu: ${temp[position]}"
+                var valor = temp[position]
+
+                if ( valor == "Celsius"){
+//                    txt_result.text = "Você escolheu: ${temp[position]}"
+                var res = calcC(valor)
+                Log.d("spn", "$res")
+                }else{
+                    txt_result.text = "Nao sei oq escolheu"
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        })
+
         btn_voltar.setOnClickListener {
             finish()
         }
 
-        btn_calc_conv.setOnClickListener {
-
-            if (isValid()){
-
-                var numb = input_numb.editableText.toString().toInt()
-
-            }else{
-                Toast.makeText(this@ConversorActivity,"Por favor preencha os valores solicitados!", Toast.LENGTH_LONG).show()
-            }
-        }
 
     }
 
     private fun isValid(): Boolean {
         return input_numb.text.toString() != ""
 
+    }
+
+    fun calcC(valor: String): String{
+                var nome = "errou"
+                var numb = input_numb.editableText.toString().toInt()
+
+//        if ( valor == "Celsius" ){
+//             nome =  "Celsius"
+//        }
+//        if (valor == "Kelvin"){
+//             nome = "Kelvin"
+//        }
+
+
+//        btn_calc_conv.setOnClickListener {
+//
+//
+//
+//            if (isValid()){
+//                if ( valor == "Celsius" ){
+//                    var nome =  "Celsius"
+//                }
+//                if (valor == "Kelvin"){
+//                    var nome = "Kelvin"
+//                }
+//
+//            }else{
+//                Toast.makeText(this@ConversorActivity,"Por favor preencha os valores solicitados!", Toast.LENGTH_LONG).show()
+//            }
+//        }
+
+
+//        return "entrou calcC"
     }
 
 }
